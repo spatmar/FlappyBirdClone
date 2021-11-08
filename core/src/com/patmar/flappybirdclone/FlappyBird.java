7cd549662b69f7f2b9743b6957e5dc1973f32395
sPatmar <spatmar@gmail.com>
2021-11-08 15:48:36 +0300

    Анимация птички

--- core/src/com/patmar/flappybirdclone/FlappyBird.java
+++ core/src/com/patmar/flappybirdclone/FlappyBird.java
@@ -10,22 +10,32 @@ public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
 
 	Texture background;
-	Texture bird_wings_up;
+	Texture[] bird;
+	int birdStateFlag = 0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("background.png");
-		bird_wings_up = new Texture("bird_wings_up.png");
+		bird = new Texture[2];
+		bird[0] = new Texture("bird_wings_up.png");
+		bird[1] = new Texture("bird_wings_down.png");
 	}
 
 	@Override
 	public void render () {
+
+		if (birdStateFlag == 0) {
+			birdStateFlag = 1;
+		} else {
+			birdStateFlag = 0;
+		}
+
 		batch.begin();
 		batch.draw(background,0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(bird_wings_up,
-				Gdx.graphics.getWidth() / 2 - bird_wings_up.getWidth() / 2,
-				Gdx.graphics.getHeight() / 2 - bird_wings_up.getHeight() / 2);
+		batch.draw(bird[birdStateFlag],
+				Gdx.graphics.getWidth() / 2 - bird[birdStateFlag].getWidth() / 2,
+				Gdx.graphics.getHeight() / 2 - bird[birdStateFlag].getHeight() / 2);
 		batch.end();
 
 	}
