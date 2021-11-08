d5849296c8bf4422229fd676ec0eb729e1b8780c
sPatmar <spatmar@gmail.com>
2021-11-08 15:36:42 +0300

    Добавление птички и выставление в центр экрана

--- core/src/com/patmar/flappybirdclone/FlappyBird.java
+++ core/src/com/patmar/flappybirdclone/FlappyBird.java
@@ -8,24 +8,26 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
+
 	Texture background;
+	Texture bird_wings_up;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("background.png");
+		bird_wings_up = new Texture("bird_wings_up.png");
 	}
 
 	@Override
 	public void render () {
 		batch.begin();
 		batch.draw(background,0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(bird_wings_up,
+				Gdx.graphics.getWidth() / 2 - bird_wings_up.getWidth() / 2,
+				Gdx.graphics.getHeight() / 2 - bird_wings_up.getHeight() / 2);
 		batch.end();
 
 	}
 	
-	@Override
-	public void dispose () {
-
-	}
 }
