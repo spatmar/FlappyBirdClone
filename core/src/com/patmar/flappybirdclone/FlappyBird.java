c4ee50fbe2b643713eccf01dd1857730497446e9
sPatmar <spatmar@gmail.com>
2021-11-08 15:26:57 +0300

    Создание фона игры

---
+++ core/src/com/patmar/flappybirdclone/FlappyBird.java
@@ -0,0 +1,31 @@
+package com.patmar.flappybirdclone;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class FlappyBird extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture background;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		background = new Texture("background.png");
+	}
+
+	@Override
+	public void render () {
+		batch.begin();
+		batch.draw(background,0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.end();
+
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
