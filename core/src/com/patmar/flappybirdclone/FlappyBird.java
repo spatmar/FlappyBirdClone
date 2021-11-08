e145c59595b01568d39386f868a214a0de67ae7f
sPatmar <spatmar@gmail.com>
2021-11-08 21:31:47 +0300

    Добавление нажатия (птица летит вверх) и падения до края экрана

--- core/src/com/patmar/flappybirdclone/FlappyBird.java
+++ core/src/com/patmar/flappybirdclone/FlappyBird.java
@@ -12,6 +12,9 @@ public class FlappyBird extends ApplicationAdapter {
 	Texture background;
 	Texture[] bird;
 	int birdStateFlag = 0;
+	float flyHeight;
+	float fallingSpeed = 0;
+	boolean gameStateFlag = false;
 	
 	@Override
 	public void create () {
@@ -20,10 +23,34 @@ public class FlappyBird extends ApplicationAdapter {
 		bird = new Texture[2];
 		bird[0] = new Texture("bird_wings_up.png");
 		bird[1] = new Texture("bird_wings_down.png");
+		flyHeight = Gdx.graphics.getHeight() / 2 - bird[0].getHeight() / 2;
+
 	}
 
 	@Override
 	public void render () {
+
+//		if (Gdx.input.justTouched()) {
+//			Gdx.app.log("Tap", "Oops!");
+//			gameStateFlag = true;
+//		}
+
+		if (gameStateFlag) {
+			if (Gdx.input.justTouched()) {
+				fallingSpeed = -30;
+			}
+			if (flyHeight > 0 || fallingSpeed < 0) {
+				fallingSpeed++;
+				flyHeight -= fallingSpeed;
+			}
+
+		} else {
+			if (Gdx.input.justTouched()) {
+				Gdx.app.log("Tap", "Oops!");
+				gameStateFlag = true;
+			}
+		}
+
 
 		if (birdStateFlag == 0) {
 			birdStateFlag = 1;
@@ -35,7 +62,7 @@ public class FlappyBird extends ApplicationAdapter {
 		batch.draw(background,0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.draw(bird[birdStateFlag],
 				Gdx.graphics.getWidth() / 2 - bird[birdStateFlag].getWidth() / 2,
-				Gdx.graphics.getHeight() / 2 - bird[birdStateFlag].getHeight() / 2);
+				flyHeight);
 		batch.end();
 
 	}
