b6f20235ab8ec0c8826bd31cfe9bcca8e2d562eb
sPatmar <spatmar@gmail.com>
2021-11-09 10:04:16 +0300

    Добавление труб

--- core/src/com/patmar/flappybirdclone/FlappyBird.java
+++ core/src/com/patmar/flappybirdclone/FlappyBird.java
@@ -15,7 +15,11 @@ public class FlappyBird extends ApplicationAdapter {
 	float flyHeight;
 	float fallingSpeed = 0;
 	boolean gameStateFlag = false;
-	
+	Texture topTube;
+	Texture bottomTube;
+	int spaceBetweenTubes = 500;
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -24,11 +28,16 @@ public class FlappyBird extends ApplicationAdapter {
 		bird[0] = new Texture("bird_wings_up.png");
 		bird[1] = new Texture("bird_wings_down.png");
 		flyHeight = Gdx.graphics.getHeight() / 2 - bird[0].getHeight() / 2;
+		topTube = new Texture("top_tube.png");
+		bottomTube = new Texture("bottom_tube.png");
 
 	}
 
 	@Override
 	public void render () {
+
+		batch.begin();
+		batch.draw(background,0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
 //		if (Gdx.input.justTouched()) {
 //			Gdx.app.log("Tap", "Oops!");
@@ -36,6 +45,13 @@ public class FlappyBird extends ApplicationAdapter {
 //		}
 
 		if (gameStateFlag) {
+
+			batch.draw(topTube,
+					Gdx.graphics.getWidth() / 2 - topTube.getWidth() / 2,
+					Gdx.graphics.getHeight() / 2 + spaceBetweenTubes / 2);
+			batch.draw(bottomTube,
+					Gdx.graphics.getWidth() / 2 - bottomTube.getWidth() / 2,
+					Gdx.graphics.getHeight() / 2 - spaceBetweenTubes / 2 - bottomTube.getHeight());
 			if (Gdx.input.justTouched()) {
 				fallingSpeed = -30;
 			}
@@ -58,8 +74,7 @@ public class FlappyBird extends ApplicationAdapter {
 			birdStateFlag = 0;
 		}
 
-		batch.begin();
-		batch.draw(background,0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
 		batch.draw(bird[birdStateFlag],
 				Gdx.graphics.getWidth() / 2 - bird[birdStateFlag].getWidth() / 2,
 				flyHeight);
